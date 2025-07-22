6a316efe9f0fd8de8a91eddfe8b894146279803a
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-07-23 00:11:42 +0800

    feat(tests): full api

--- api_test.go
+++ api_test.go
@@ -35,10 +35,59 @@ func TestPing(t *testing.T) {
 	assert.Equal(t, 1752932834, ping.Data.Request.Ts)
 }
 
-func TestQueryOrder_Error(t *testing.T) {
+func TestQueryRandomReply(t *testing.T) {
 	t.Parallel()
 
-	mockRT := &mockRoundTripper{
+	reply, err := NewClient(&Config{UserID: "user123", APIToken: "token123"}, &http.Client{
+		Transport: &mockRoundTripper{
+			roundTrip: func(req *http.Request) (*http.Response, error) {
+				assert.Equal(t, http.MethodPost, req.Method)
+
+				return &http.Response{
+					StatusCode: http.StatusOK,
+					Body:       io.NopCloser(bytes.NewBufferString(`{"ec":200,"em":"success","data":{"list":[{"out_trade_no":"202505141538455397541020050","content":"999"}]}}`)),
+					Header:     make(http.Header),
+				}, nil
+			},
+		},
+	}).QueryRandomReply("202505141538455397541020050")
+
+	require.NoError(t, err)
+	assert.NotNil(t, reply)
+	assert.Equal(t, 200, reply.EC)
+	assert.Equal(t, "ok", reply.EM)
+	assert.NotEmpty(t, reply.Data.List)
+}
+
+func TestQuerySponsor(t *testing.T) {
+	t.Parallel()
+
+	sponsor, err := NewClient(&Config{UserID: "user123", APIToken: "token123"}, &http.Client{
+		Transport: &mockRoundTripper{
+			roundTrip: func(req *http.Request) (*http.Response, error) {
+				assert.Equal(t, http.MethodPost, req.Method)
+
+				return &http.Response{
+					StatusCode: http.StatusOK,
+					Body:       io.NopCloser(bytes.NewBufferString(`{"ec":200,"em":"","data":{"list":[{"out_trade_no":"202106232138371083454010626","custom_order_id":"Steam12345","user_id":"adf397fe8374811eaacee52540025c377","user_private_id":"33","plan_id":"a45353328af911eb973052540025c377","month":1,"total_amount":"5.00","show_amount":"5.00","status":2,"remark":"","redeem_id":"","product_type":0,"discount":"0.00","sku_detail":[{"sku_id":"b082342c4aba11ebb5cb52540025c377","count":1,"name":"15000 赏金/货币 兑换码","album_id":"","pic":"https://pic1.afdiancdn.com/user/8a8e408a3aeb11eab26352540025c377/common/sfsfsff.jpg"}],"address_person":"","address_phone":"","address_address":""}],"total_count":167,"total_page":11}}`)),
+					Header:     make(http.Header),
+				}, nil
+			},
+		},
+	}).QuerySponsor(1, 100)
+
+	require.NoError(t, err)
+	assert.NotNil(t, sponsor)
+	assert.Equal(t, 200, sponsor.EC)
+	assert.Equal(t, "ok", sponsor.EM)
+	assert.Equal(t, 1, sponsor.Data.TotalCount)
+	assert.NotEmpty(t, sponsor.Data.List)
+}
+
+func TestQueryOrderError(t *testing.T) {
+	t.Parallel()
+
+	order, err := NewClient(&Config{UserID: "user123", APIToken: "token123"}, &http.Client{Transport: &mockRoundTripper{
 		roundTrip: func(req *http.Request) (*http.Response, error) {
 			return &http.Response{
 				StatusCode: http.StatusOK,
@@ -46,16 +95,28 @@ func TestQueryOrder_Error(t *testing.T) {
 				Header:     make(http.Header),
 			}, nil
 		},
-	}
-
-	mockHTTPClient := &http.Client{Transport: mockRT}
-	cfg := &Config{UserID: "user123", APIToken: "token123"}
-	client := NewClient(cfg, mockHTTPClient)
-
-	order, err := client.QueryOrder(1, 10)
+	}}).QueryOrder(1, 10)
 
 	require.NoError(t, err)
 	assert.NotNil(t, order)
 	assert.Equal(t, 400, order.EC)
 	assert.Equal(t, "error message from server", order.EM)
+}
+
+func TestDoRequestInvalidJSON(t *testing.T) {
+	t.Parallel()
+
+	ping, err := NewClient(&Config{UserID: "user123", APIToken: "token123"}, &http.Client{Transport: &mockRoundTripper{
+		roundTrip: func(req *http.Request) (*http.Response, error) {
+			return &http.Response{
+				StatusCode: http.StatusOK,
+				Body:       io.NopCloser(bytes.NewBufferString(`{"ec":200,"em":"ok","data":}`)),
+				Header:     make(http.Header),
+			}, nil
+		},
+	}}).Ping()
+
+	require.Error(t, err)
+	assert.NotNil(t, ping)
+	assert.Equal(t, 0, ping.EC)
 }
