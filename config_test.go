ce7c8d0df29075be5ea7e3a40a5e8508ef3b9250
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-07-22 22:34:37 +0800

    feat(tests): custom base url & webhook path config

--- config_test.go
+++ config_test.go
@@ -17,12 +17,28 @@ func TestConfig_setDefaults(t *testing.T) {
 		assert.Equal(t, "https://afdian.com/api", cfg.BaseURL)
 	})
 
+	t.Run("custom base url", func(t *testing.T) {
+		t.Parallel()
+
+		cfg := &Config{BaseURL: "https://custom.url"}
+		cfg.setDefaults()
+		assert.Equal(t, "https://custom.url", cfg.BaseURL)
+	})
+
 	t.Run("default webhook path", func(t *testing.T) {
 		t.Parallel()
 
 		cfg := &Config{WebHookListenAddr: ":8080"}
 		cfg.setDefaults()
 		assert.Equal(t, "/", cfg.WebHookPath)
+	})
+
+	t.Run("custom webhook path", func(t *testing.T) {
+		t.Parallel()
+
+		cfg := &Config{WebHookListenAddr: ":8080", WebHookPath: "/hook"}
+		cfg.setDefaults()
+		assert.Equal(t, "/hook", cfg.WebHookPath)
 	})
 
 	t.Run("no default webhook path", func(t *testing.T) {
