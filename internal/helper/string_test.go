7e20a27a63f73f2a183beb6130c9322f252539dd
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-07-22 22:34:59 +0800

    feat(tests): default string

--- internal/helper/string_test.go
+++ internal/helper/string_test.go
@@ -34,23 +34,48 @@ func TestBytesToString(t *testing.T) {
 
 		assert.Equal(t, string(long), BytesToString(long))
 	})
-	t.Run("test non-empty bytes", func(t *testing.T) {
+
+	t.Run("test default bytes", func(t *testing.T) {
 		t.Parallel()
-		assert.Equal(t, "test", BytesToString([]byte("test")))
+		assert.Equal(t, "0D000721, Ciallo～(∠・ω< )⌒★", BytesToString([]byte("0D000721, Ciallo～(∠・ω< )⌒★")))
 	})
 }
 
 func TestStringToBytes(t *testing.T) {
 	t.Parallel()
-	assert.Nil(t, StringToBytes(""))
-	assert.Equal(t, []byte("hello"), StringToBytes("hello"))
-	assert.Equal(t, []byte("你好，世界"), StringToBytes("你好，世界")) //nolint:gosmopolitan
-	assert.Equal(t, []byte("!@#$%^&*()_+"), StringToBytes("!@#$%^&*()_+"))
 
-	long := make([]byte, 10000)
-	for i := range long {
-		long[i] = 'a'
-	}
+	t.Run("empty string", func(t *testing.T) {
+		t.Parallel()
+		assert.Nil(t, StringToBytes(""))
+	})
 
-	assert.Equal(t, long, StringToBytes(string(long)))
+	t.Run("ascii string", func(t *testing.T) {
+		t.Parallel()
+		assert.Equal(t, []byte("hello"), StringToBytes("hello"))
+	})
+
+	t.Run("unicode string", func(t *testing.T) {
+		t.Parallel()
+		assert.Equal(t, []byte("你好，世界"), StringToBytes("你好，世界")) //nolint:gosmopolitan
+	})
+
+	t.Run("special chars string", func(t *testing.T) {
+		t.Parallel()
+		assert.Equal(t, []byte("!@#$%^&*()_+"), StringToBytes("!@#$%^&*()_+"))
+	})
+
+	t.Run("long string", func(t *testing.T) {
+		t.Parallel()
+		long := make([]byte, 10000)
+		for i := range long {
+			long[i] = 'a'
+		}
+
+		assert.Equal(t, long, StringToBytes(string(long)))
+	})
+
+	t.Run("default string", func(t *testing.T) {
+		t.Parallel()
+		assert.Equal(t, []byte("0D000721, Ciallo～(∠・ω< )⌒★"), StringToBytes("0D000721, Ciallo～(∠・ω< )⌒★"))
+	})
 }
