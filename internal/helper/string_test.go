6fcc3ae6f77afe20f3bfb290897f4205d6183dff
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-07-27 21:06:38 +0800

    fix(tests): dupe code

--- internal/helper/string_test.go
+++ internal/helper/string_test.go
@@ -8,22 +8,52 @@ import (
 
 func TestBytesToString(t *testing.T) {
 	t.Parallel()
-	t.Run("test empty bytes", func(t *testing.T) {
-		t.Parallel()
-		assert.Empty(t, BytesToString(nil))
-	})
-	t.Run("test ascii bytes", func(t *testing.T) {
-		t.Parallel()
-		assert.Equal(t, "hello", BytesToString([]byte("hello")))
-	})
-	t.Run("test unicode bytes", func(t *testing.T) {
-		t.Parallel()
-		assert.Equal(t, "你好，世界", BytesToString([]byte("你好，世界"))) //nolint:gosmopolitan
-	})
-	t.Run("test special chars", func(t *testing.T) {
-		t.Parallel()
-		assert.Equal(t, "!@#$%^&*()_+", BytesToString([]byte("!@#$%^&*()_+")))
-	})
+
+	testCases := []struct {
+		name  string
+		input []byte
+		want  string
+	}{
+		{
+			name:  "nil bytes",
+			input: nil,
+			want:  "",
+		},
+		{
+			name:  "empty bytes",
+			input: []byte{},
+			want:  "",
+		},
+		{
+			name:  "ascii bytes",
+			input: []byte("hello"),
+			want:  "hello",
+		},
+		{
+			name:  "unicode bytes",
+			input: []byte("你好，世界"), //nolint:gosmopolitan
+			want:  "你好，世界",
+		},
+		{
+			name:  "special chars",
+			input: []byte("!@#$%^&*()_+"),
+			want:  "!@#$%^&*()_+",
+		},
+		{
+			name:  "default bytes",
+			input: []byte("0D000721, Ciallo～(∠・ω< )⌒★"),
+			want:  "0D000721, Ciallo～(∠・ω< )⌒★",
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			assert.Equal(t, tc.want, BytesToString(tc.input))
+		})
+	}
+
 	t.Run("test long bytes", func(t *testing.T) {
 		t.Parallel()
 
@@ -34,35 +64,54 @@ func TestBytesToString(t *testing.T) {
 
 		assert.Equal(t, string(long), BytesToString(long))
 	})
-
-	t.Run("test default bytes", func(t *testing.T) {
-		t.Parallel()
-		assert.Equal(t, "0D000721, Ciallo～(∠・ω< )⌒★", BytesToString([]byte("0D000721, Ciallo～(∠・ω< )⌒★")))
-	})
 }
 
 func TestStringToBytes(t *testing.T) {
 	t.Parallel()
 
-	t.Run("empty string", func(t *testing.T) {
-		t.Parallel()
-		assert.Nil(t, StringToBytes(""))
-	})
+	testCases := []struct {
+		name  string
+		input string
+		want  []byte
+	}{
+		{
+			name:  "empty string",
+			input: "",
+			want:  nil,
+		},
+		{
+			name:  "ascii string",
+			input: "hello",
+			want:  []byte("hello"),
+		},
+		{
+			name:  "unicode string",
+			input: "你好，世界", //nolint:gosmopolitan
+			want:  []byte("你好，世界"),
+		},
+		{
+			name:  "special chars string",
+			input: "!@#$%^&*()_+",
+			want:  []byte("!@#$%^&*()_+"),
+		},
+		{
+			name:  "default string",
+			input: "0D000721, Ciallo～(∠・ω< )⌒★",
+			want:  []byte("0D000721, Ciallo～(∠・ω< )⌒★"),
+		},
+	}
 
-	t.Run("ascii string", func(t *testing.T) {
-		t.Parallel()
-		assert.Equal(t, []byte("hello"), StringToBytes("hello"))
-	})
-
-	t.Run("unicode string", func(t *testing.T) {
-		t.Parallel()
-		assert.Equal(t, []byte("你好，世界"), StringToBytes("你好，世界")) //nolint:gosmopolitan
-	})
-
-	t.Run("special chars string", func(t *testing.T) {
-		t.Parallel()
-		assert.Equal(t, []byte("!@#$%^&*()_+"), StringToBytes("!@#$%^&*()_+"))
-	})
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			if tc.input == "" {
+				assert.Nil(t, StringToBytes(tc.input))
+			} else {
+				assert.Equal(t, tc.want, StringToBytes(tc.input))
+			}
+		})
+	}
 
 	t.Run("long string", func(t *testing.T) {
 		t.Parallel()
@@ -73,10 +122,5 @@ func TestStringToBytes(t *testing.T) {
 		}
 
 		assert.Equal(t, long, StringToBytes(string(long)))
-	})
-
-	t.Run("default string", func(t *testing.T) {
-		t.Parallel()
-		assert.Equal(t, []byte("0D000721, Ciallo～(∠・ω< )⌒★"), StringToBytes("0D000721, Ciallo～(∠・ω< )⌒★"))
 	})
 }
