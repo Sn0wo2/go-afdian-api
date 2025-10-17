9de93b922be95e109a50e7a6fc65cf5b0bd733ad
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-10-17 11:50:16 +0800

    chore(debug): init

---
+++ internal/debug/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/Sn0wo2/go-afdian-api"
+)
+
+func main() {
+	config := &afdian.Config{
+		UserID:   os.Getenv("USER_ID"),
+		APIToken: os.Getenv("API_TOKEN"),
+
+		/*WebHookListenAddr: ":9000",
+		  WebHookPath:       "/WH",
+		  WebHookCallback: func(p *payload.WebHook, errs ...error) {
+		  	body, err := io.ReadAll(p.RawRequest.Body)
+		  	if err != nil {
+		  		panic(err)
+		  	}
+		  	fmt.Println(helper.BytesToString(body))
+		  },*/
+	}
+	client := afdian.NewClient(config)
+
+	sponsor, err := client.QuerySponsor(1, 100)
+	if err != nil {
+		panic(err)
+	}
+
+	body, err := io.ReadAll(sponsor.RawResponse.Body)
+	fmt.Println(string(body))
+
+	/*if err := afdian.NewWebHook(client).Start(); err != nil {
+		panic(err)
+	}*/
+}
